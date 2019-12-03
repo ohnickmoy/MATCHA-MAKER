8a94e93d7e724733ccd2c24ec206db3a014d082e
Nick <nick.moi@gmail.com>
2019-12-03 16:11:47 -0500

    Back end functionality

--- Matcha_maker_backend/app/controllers/api/v1/users_controller.rb
+++ Matcha_maker_backend/app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,15 @@ class Api::V1::UsersController < ApplicationController
         users = User.all
         render json: UserSerializer.new(users)
     end
+
+    def show
+        user = User.find(params[:id])
+        render json: UserSerializer.new(user)
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(matchas: params[:matchas])
+        render json: UserSerializer.new(user)
+    end
 end
