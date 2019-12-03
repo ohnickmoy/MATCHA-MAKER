dd736c06c50c8be0a43d6bdfb656eda6a6c75831
github username <github email address>
2019-12-03 16:12:49 -0500

    added shop and icon window logic

---
+++ test.js
@@ -0,0 +1,10 @@
+<div class="maker-container">
+    <div class="maker-image">
+        <img src="images/matcha-maker-logo.png" id="maker-image">
+    </div>
+    <div style="flex-grow: 8" class="maker-content">
+        <div>name</div>
+        <div>desc</div>
+        <div>cost/count</div>
+    </div>
+</div>
