fd161a7bd6f17c3188ba3efb9693c1f6aa9cb83f
github username <github email address>
2019-12-04 09:26:07 -0500

    CSS Work and Shop Button Functions

--- index.js
+++ index.js
@@ -21,45 +21,43 @@ function increaseMatchaCount(){
     //console.log(lifetimeMatchaCounter)
 }
 
-function appendMaker(){
-    switch(lifetimeMatchaCounter){
-        case 10:
-            //console.log('I made it mom')
-            let cursorContainer = document.createElement('div')
-            cursorContainer.classList.add('maker-container')
-            cursorContainer.innerHTML = `
-            <div class="maker-image">
-                <img src="images/matcha-maker-logo.png" id="maker-image" data-type="cursor-btn">
-            </div>
-            <div style="flex-grow: 8" class="maker-content" data-type="cursor-btn">
-                <div>Cursors: <span id="cursorCount">0</span></div>
-                <div>Cursor Cost: <span id="cursorCost">15</span></div>
-            </div>
-            `
-            cursorContainer.dataset.type = 'cursor-btn'
-            cursorDiv.append(cursorContainer)
-            break;
-            // let cursorImg = document.createElement('img')
-            // let cursorCount = document.createElement('p')
-            // let cursorCost = document.createElement('p')
-            // cursorCount.innerHTML = `Cursors: <span id="cursorCount">0</span><br>`
-            // cursorCost.innerHTML = `Cursor Cost: <span id="cursorCost">15</span><br>`
-            // cursorImg.src = "images/placeholder-01.png"
-            // cursorImg.dataset.type = 'cursor-btn'
-            // cursorImg.classList.add("image")
-            // cursorDiv.append(cursorImg)
-            // cursorDiv.append(cursorCount)
-            // cursorDiv.append(cursorCost)
-            // break;
-        case 15:
-            console.log('er?')
-            let kenImg = document.createElement('img')
-            kenImg.src = "images/placeholder-01.png"
-            kenImg.classList.add("image")
-            kenDiv.append(kenImg)
-            break;
+
+//this is the function for the adding shop buttons
+let cursorButton = document.querySelector("#cursor")
+cursorButton.style.display = "none";
+
+function shopButtons(){
+    if(lifetimeMatchaCounter >= 10){
+        cursorButton.style.display = "flex"
+    }
+    
+}
+
+
+//these are the functions for adding the icon holders
+let cursorIconHolder = document.querySelector("body > div > div.column.middle > div")
+cursorIconHolder.style.display = "none"
+let middleColumn = document.querySelector("body > div > div.column.middle")
+
+function iconHolders(){
+    let cursorCounter = document.querySelector("#cursorCount")
+    cursorCounter = parseInt(cursorCounter.innerText)
+    if(cursorCounter === 1){
+        cursorIconHolder.style.display = ""
     }
 }
+
+// function appendMaker(){
+//     switch(lifetimeMatchaCounter){
+//         case 15:
+//             console.log('er?')
+//             let kenImg = document.createElement('img')
+//             kenImg.src = "images/placeholder-01.png"
+//             kenImg.classList.add("image")
+//             kenDiv.append(kenImg)
+//             break;
+//     }
+// }
 
 function persistMatchaCount(id, matchas){
     fetch(`http://localhost:3000/api/v1/users/${id}`, {method: "PATCH", 
@@ -74,7 +72,7 @@ function persistMatchaCount(id, matchas){
 matchaCup.addEventListener('click', function(e){
     increaseMatchaCount()
     persistMatchaCount(e.target.dataset.userId, e.target.dataset.userMatchas)
-    appendMaker()
+    shopButtons()
 })
 
 setInterval(function(){
@@ -99,25 +97,14 @@ rightColumn.addEventListener('click', function(e){
             mps = mps + 0.2;
             mpsCounter.innerText = mps.toPrecision(2)
             cursorCostCounter.innerText = Math.round(15 * Math.pow(1.1, parseInt(cursorCounter.innerText)))
-            cursorIconAppender()
             matchaCup.dataset.userMatchas = matchaCount
             persistMatchaCount(matchaCup.dataset.userId, matchaCup.dataset.userMatchas)
+            iconHolders()
         }
     }
 })
 
-let cursorIconHolder = document.createElement("div")
-cursorIconHolder.className = "icon-container"
-let middleColumn = document.querySelector("body > div > div.column.middle")
 
-function cursorIconAppender(){
-    let cursorCounter = document.querySelector("#cursorCount")
-    cursorCounter = parseInt(cursorCounter.innerText)
-    if(cursorCounter === 1){
-        middleColumn.append(cursorIconHolder)
-        console.log("I got here!")
-    }
-}
 //fetch get request for user info, hard coded to nick
 function loadUserInfo(json){
     shopName.innerText = `${json.data.attributes.name}'s Matcha Shop!`
